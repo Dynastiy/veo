0cf929ed39e74197460b75fdfcc5412d7ead780b
Dynastiy <emmanuel00michael@gmail.com>
2022-05-20 07:03:51 +0100

    fixed signin options

--- src/views/auth/signInOptions.vue
+++ src/views/auth/signInOptions.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="sign-in-options">
-            <div>
+            <div class="container">
                 
                 <div class="text-center mb-4">
                     <img src="@/assets/img/logo.svg" alt="" srcset="">
@@ -10,10 +10,10 @@
             <div class="options">
                 <button class="choose--option" @click="goToHospital">
                 Sign in as Hospital
-            </button>
-             <button class="choose--option" @click="goToCorporate">
-                Sign in as Corporate Body
-            </button>
+                </button>
+                <button class="choose--option" @click="goToCorporate">
+                    Sign in as Corporate Body
+                </button>
             </div>
             </div>
         </div>
